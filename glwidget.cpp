2fd61c13429350f0a4816d895818789f00574fae
Tom Lou <louyu27@cs.washington.edu>
2020-07-02 01:08:43 -0500

    Draw transparent lines

--- glwidget.cpp
+++ glwidget.cpp
@@ -162,11 +162,11 @@ static const char *fragmentShaderSourceCore =
     "out highp vec4 fragColor;\n"
     "uniform highp vec3 lightPos;\n"
     "void main() {\n"
-    "   highp vec3 L = normalize(lightPos - vert);\n"
-    "   highp float NL = max(dot(normalize(vertNormal), L), 0.0);\n"
-    "   highp vec3 color = vec3(0.39, 1.0, 0.0);\n"
-    "   highp vec3 col = clamp(color * 0.2 + color * 0.8 * NL, 0.0, 1.0);\n"
-    "   fragColor = vec4(col, 1.0);\n"
+    "   // highp vec3 L = normalize(lightPos - vert);\n"
+    "   // highp float NL = max(dot(normalize(vertNormal), L), 0.0);\n"
+    "   // highp vec3 color = vec3(0.39, 1.0, 0.0);\n"
+    "   // highp vec3 col = clamp(color * 0.2 + color * 0.8 * NL, 0.0, 1.0);\n"
+    "   fragColor = vec4(color, 0.5);\n"
     "}\n";
 
 static const char *vertexShaderSource =
@@ -188,11 +188,11 @@ static const char *fragmentShaderSource =
     "varying highp vec3 vertNormal;\n"
     "uniform highp vec3 lightPos;\n"
     "void main() {\n"
-    "   highp vec3 L = normalize(lightPos - vert);\n"
-    "   highp float NL = max(dot(normalize(vertNormal), L), 0.0);\n"
-    "   highp vec3 color = vec3(0.39, 1.0, 0.0);\n"
-    "   highp vec3 col = clamp(color * 0.2 + color * 0.8 * NL, 0.0, 1.0);\n"
-    "   gl_FragColor = vec4(col, 1.0);\n"
+    "   // highp vec3 L = normalize(lightPos - vert);\n"
+    "   // highp float NL = max(dot(normalize(vertNormal), L), 0.0);\n"
+    "   // highp vec3 color = vec3(0.39, 1.0, 0.0);\n"
+    "   // highp vec3 col = clamp(color * 0.2 + color * 0.8 * NL, 0.0, 1.0);\n"
+    "   gl_FragColor = vec4(color, 0.5);\n"
     "}\n";
 
 void GLWidget::initializeGL()
@@ -241,6 +241,10 @@ void GLWidget::initializeGL()
     m_program->setUniformValue(m_lightPosLoc, QVector3D(0, 0, 70));
 
     m_program->release();
+
+    // Enable transparency
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 }
 
 void GLWidget::setupVertexAttribs()
